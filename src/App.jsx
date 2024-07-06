c7a458c494c280fa0b3426b7e2cbf94095b9e10c
mahmutcant <mahcan012@gmail.com>
2024-07-06 16:36:48 +0300

    created project tailwind added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <div className="h-full">
+        <span className="flex items-center justify-center">recipe</span>
+      </div>
+    </>
+  )
+}
+
+export default App
